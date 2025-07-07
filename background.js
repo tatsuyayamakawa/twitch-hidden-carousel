13c3181bdd2f0686f631be1af022b45f011a982d
tatsuyayamakawa <t.yamakawa0121@gmail.com>
2025-07-07 15:04:24 +0900

    remove: background.js, images directory, and style.css

--- background.js
+++
@@ -1,8 +0,0 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-	if (tab.url.indexOf('https://www.twitch.tv/*') > -1) {
-		chrome.scripting.insertCSS({
-			target: { tabId: tab.id },
-			files: ['style.css'],
-		});
-	}
-});
